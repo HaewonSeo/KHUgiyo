0efc8c9b1b7e2ed929f2b36fc6e868b7f2912ea5
HaewonSeo <50014877+HaewonSeo@users.noreply.github.com>
2019-11-21 20:57:15 +0900

    Add files via upload

--- Order/Order.h
+++ Order/Order.h
@@ -8,12 +8,9 @@ using namespace std;
 
 class Order {
 public:
-	Order(string clientid="null")
-	{
-		oId = clientid;
-		SetOrderTotalPrice();
-		SetOrderTime();
-	}
+
+	//Constructor
+	Order(string clientid = "null");
 
 	
 	/**
@@ -23,9 +20,7 @@ public:
 	*	@param	none
 	*	@return order한 client의 id를 반환
 	*/
-	string GetOrderId() const {
-		return oId;
-	}
+	string GetOrderId() const;
 
 	/**
 	*	@brief	Order의 총합을 설정 (MenuList내의 메뉴의 총합을 반환하여)
@@ -34,9 +29,7 @@ public:
 	*	@param	none
 	*	@return Order의 총합을 설정
 	*/
-	void SetOrderTotalPrice() {
-		oTotalPrice = menuList.GetTotal();
-	}
+	void SetOrderTotalPrice();
 
 	/**
 	*	@brief	Order의 총합을 반환
@@ -45,10 +38,7 @@ public:
 	*	@param	none
 	*	@return Order의 총합을 반환
 	*/
-	int GetOrderTotalPrice() const {
-		return oTotalPrice;
-	}
-
+	int GetOrderTotalPrice() const;
 	/**
 	*	@brief	Order한 시간을 설정
 	*	@pre	order 객체의 생성
@@ -56,9 +46,7 @@ public:
 	*	@param	none
 	*	@return none
 	*/
-	void SetOrderTime() {
-		oTime = time(NULL);	
-	}
+	void SetOrderTime();
 
 	/**
 	*	@brief	Order한 시간을 출력
@@ -67,13 +55,7 @@ public:
 	*	@param	none
 	*	@return none
 	*/
-	void PrintOrderTime() {
-		struct tm *date;
-		date = localtime(&oTime);
-		//년/월/일 시:분:초 형식으로 출력
-		printf("%d/%d/%d %d:%d:%d\n", date->tm_year + 1900, date->tm_mon + 1, date->tm_mday, date->tm_hour, date->tm_min, date->tm_sec);
-	}
-
+	void PrintOrderTime();
 	/**
 	*	@brief	Order한 시간을 time_t 형식으로 반환
 	*	@pre	order 객체의 생성
@@ -81,9 +63,8 @@ public:
 	*	@param	none
 	*	@return Order한 시간을 반환
 	*/
-	time_t GetOrderTime() const {
-		return oTime;
-	}
+	time_t GetOrderTime() const;
+	
 
 	/**
 	*	@brief	Order의 정보를 출력
@@ -92,12 +73,7 @@ public:
 	*	@param	none
 	*	@return Order의 정보를 출력
 	*/
-	void PrintOrder() {
-		cout << endl;
-		cout << "### Order ID : " << oId << endl;
-		cout << "### Order Total Price : " << oTotalPrice << endl;
-		menuList.PrintMenuList();
-	}
+	void PrintOrder() const;
 
 	
 
